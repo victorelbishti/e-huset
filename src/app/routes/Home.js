bdaa44e135f12b451e8e74eaf52185c1005e9d96
DESKTOP-JLMNFTE\victo <victorelbishti@gmail.com>
2018-06-07 09:22:53 +0200

    New homepage element

--- src/app/routes/Home.js
+++ src/app/routes/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import {SectionDiv, Grid, GridCell, size} from "../components/Styled";
-import HouseImg from "../images/house.png";
+import Icon from "../components/Icon";
 
 const Section = styled.section`
     height: 100vh;
@@ -26,21 +26,58 @@ const Image = styled.img`
     }
 `;
 
+const ListItem = styled.li`
+    list-style-type: none;
+    display: block;
+    color: #fff;
+    margin: 10px 20px;
+    
+    span svg {
+        display: inline;
+        fill: #fff;
+        margin-right: 20px;
+        vertical-align: text-bottom;
+    }
+`;
+
+const List = styled.ul`
+    padding: 0;
+    width: 350px;
+    margin: 0 auto;
+`;
+
 class Home extends Component {
     render() {
         return (
             <Section id="home">
                 <SectionDiv responsive={"margin: 0 20px; padding-top: 100px"}>
                     <Grid columns={"1fr 1fr"}>
-                        <GridCell>
+                        <GridCell responsive={"padding: 0"}>
                             <h2>Få ordning på din digitala profil</h2>
                             <Text>Låt e-huset hjälpa dig, vi är experter på webbutveckling och digital
                                 marknadsföring. Oavsett om du är i behov av en ny hemsida, en digital
                                 renovering eller några timmar support i månaden så finns vi här för att
                                 hjälpa dig. Scrolla ner för att se våra erbjudanden.</Text>
                         </GridCell>
-                        <GridCell align={"center"} responsive={"display: none;"}>
-                            <Image src={HouseImg} alt={"E-huset.se"} />
+                        <GridCell responsive={"padding: 0"}>
+                            <List>
+                                <ListItem>
+                                    <span><Icon size={"2em"} icon={"checkmark"} /></span>
+                                    <span>Responsiva hemsidor</span>
+                                </ListItem>
+                                <ListItem>
+                                    <span><Icon size={"2em"} icon={"checkmark"} /></span>
+                                    <span>Löpande underhåll</span>
+                                </ListItem>
+                                <ListItem>
+                                    <span><Icon size={"2em"} icon={"checkmark"} /></span>
+                                    <span>Sökoptimering</span>
+                                </ListItem>
+                                <ListItem>
+                                    <span><Icon size={"2em"} icon={"checkmark"} /></span>
+                                    <span>Digital marknadsföring</span>
+                                </ListItem>
+                            </List>
                         </GridCell>
                     </Grid>
                 </SectionDiv>
